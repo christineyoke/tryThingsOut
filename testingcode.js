cc402e65f529fa3414572f4dcae29ee36b7eea19
Christine Yoke <christineyoke@yahoo.com>
2024-01-19 19:01:20 -0500

    This is CSS and HTML to create an email form

---
+++ testingcode.js
@@ -0,0 +1,34 @@
+const locations = {
+  "40.35": {
+    "-105.81": "ROCKY-MOUNTAINS",
+    "22.54": "KATERINI-GREECE"
+  },
+  "28.66": {
+    "-105.81": "CHIHUAHUA-MEXICO"
+  }
+};
+
+function goToSecretLocation(lat, lon, passcode) {
+  if (!lat || !lon || !passcode) {
+    throw "latitude, longitude, and passcode are required!";
+  }
+
+  let result;
+  try {
+    if (locations[lat][lon] === passcode) {
+      result = "Welcome.";
+    } else {
+      throw "Nothing to see here!";
+    }
+  } catch (error) {
+    if (typeof error === "string") {
+      result = error;
+    } else {
+      result = "You look around and don't see a thing.";
+    }
+  }
+
+  return result;
+}
+
+goToSecretLocation(40.35, 22.54, "KATERINI-GREECE");
